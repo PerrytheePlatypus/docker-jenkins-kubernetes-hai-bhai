c9ee58e64b13b2e0a89c84ccd913034e78d44172
Aditya singhal <133752891+aditya-blanko@users.noreply.github.com>
2025-04-15 12:23:23 +0530

    Add files via upload

---
+++ Terraform/main.tf
@@ -0,0 +1,47 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "bf9c4a23-fb6a-43a2-a6c4-9e224b69b5ac" 
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "rg-aks-acr"
+  location = "East US 2"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "kubernetes-acr-04082003" 
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "my-cluster-04082003"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "myaksdns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "Standard"
+  }
+
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
