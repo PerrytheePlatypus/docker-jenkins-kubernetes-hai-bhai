f7191c247984f04ca985b92269afe01d83a85955
Aditya singhal <133752891+aditya-blanko@users.noreply.github.com>
2025-04-16 20:03:58 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -94,15 +94,4 @@ resource "azurerm_role_assignment" "aks_acr_pull" {
   ]
 }
 
-# Outputs
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
 
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
-
-output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks.name
-}
